faa4ecf1a2df30a8be270074a7e930e56f67a541
Kgermando <katakugermain@gmail.com>
2023-08-15 19:31:07 +0100

    ffj

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ const env = process.env.NODE_ENV;
 
 export const environment = {  
   apiURL: (env  === 'production') 
-    ? 'https://mpox-api-7c81d2a0cb87.herokuapp.com/api'
+    ? 'https://wmrh-e189cd6cadd5.herokuapp.com/api'
     : 'http://localhost:3000/api'
 };
