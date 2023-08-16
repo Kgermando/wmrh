11fcd9b9a4f82034f305c83929bfd0b6d28c38cf
Kgermando <katakugermain@gmail.com>
2023-08-16 15:54:10 +0100

    fixing auth

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -53,65 +53,64 @@ export class LoginComponent {
             let user: PersonnelModel = res; 
             let roleList = JSON.stringify(user.roles);
             localStorage.removeItem('roles');
-            localStorage.setItem('roles', roleList);
-            this.router.navigate(['/layouts/dashboard']);  
-            // if (user.statut_personnel) {
-            //   if (user.roles[0] === 'Dashboard') { 
-            //     this.router.navigate(['/layouts/dashboard']);  
-            //   } else if (user.roles[0] === 'Personnels') { 
-            //     this.router.navigate(['/layouts/personnels/personnel-list']); 
-            //   } else if (user.roles[0] === 'Pointages') {
-            //     this.router.navigate(['/layouts/presences/pointage']);
-            //   } else if (user.roles[0] === 'Registre de presences') {
-            //     this.router.navigate(['/layouts/presences/registre-presences']);
-            //   } else if (user.roles[0] === 'Heure suplementaires') {
-            //     this.router.navigate(['/layouts/presences/heures-supp']);
-            //   } else if (user.roles[0] === 'Liste de paiements') {
-            //     this.router.navigate(['/layouts/salaires/liste-paiements']);
-            //   } else if (user.roles[0] === 'Statuts de paies') {
-            //     this.router.navigate(['/layouts/salaires/statuts-paies']);
-            //   } else if (user.roles[0] === 'Relevés de paies') {
-            //     this.router.navigate(['/layouts/salaires/releve-paie']);
-            //   } else if (user.roles[0] === 'Avances salaires') {
-            //     this.router.navigate(['/layouts/salaires/avance-salaire']);
-            //   } else if (user.roles[0] === 'Primes divers') {
-            //     this.router.navigate(['/layouts/salaires/primes']);
-            //   } else if (user.roles[0] === 'Pénalites') {
-            //     this.router.navigate(['/layouts/salaires/penalites']);
-            //   } else if (user.roles[0] === 'Près entreprise') {
-            //     this.router.navigate(['/layouts/salaires/pres-entreprise']);
-            //   } else if (user.roles[0] === 'Syndicats') {
-            //     this.router.navigate(['/layouts/personnels/syndicats']);
-            //   } else if (user.roles[0] === 'Horaires') {
-            //     this.router.navigate(['/layouts/horaire']);
-            //   } else if (user.roles[0] === 'Performences') {
-            //     this.router.navigate(['/layouts/performences']);
-            //   } else if (user.roles[0] === 'Postes') {
-            //     this.router.navigate(['/layouts/recrutements/postes']);
-            //   } else if (user.roles[0] === 'Candidatures') {
-            //     this.router.navigate(['/layouts/recrutements/candidatures']);
-            //   } else if (user.roles[0] === 'Emails') {
-            //     this.router.navigate(['/layouts/mail/inbox']);
-            //   } else if (user.roles[0] === 'Preferences') {
-            //     this.router.navigate(['/layouts/preferences/fonction']);
-            //   } else if (user.roles[0] === 'Reglages') {
-            //     this.router.navigate(['/layouts/preferences/reglages']);
-            //   } else if (user.roles[0] === 'Departements') {
-            //     this.router.navigate(['/layouts/preferences/departement']);
-            //   } else if (user.roles[0] === 'Sites location') {
-            //     this.router.navigate(['/layouts/preferences/site-location']);
-            //   } else if (user.roles[0] === 'Fonctions') {
-            //     this.router.navigate(['/layouts/preferences/fonction']);
-            //   } else if (user.roles[0] === 'Services') {
-            //     this.router.navigate(['/layouts/preferences/services']);
-            //   } else if (user.roles[0] === 'Titres') {
-            //     this.router.navigate(['/layouts/preferences/titles']);
-            //   } else if (user.roles[0] === 'Archives') {
-            //     this.router.navigate(['/layouts/archives']);
-            //   }
-            // } else {
-            //   this.router.navigate(['/auth/login']);
-            // }
+            localStorage.setItem('roles', roleList); 
+            if (user.statut_personnel) {
+              if (user.roles[0] === 'Dashboard') { 
+                this.router.navigate(['/layouts/dashboard']);  
+              } else if (user.roles[0] === 'Personnels') { 
+                this.router.navigate(['/layouts/personnels/personnel-list']); 
+              } else if (user.roles[0] === 'Pointages') {
+                this.router.navigate(['/layouts/presences/pointage']);
+              } else if (user.roles[0] === 'Registre de presences') {
+                this.router.navigate(['/layouts/presences/registre-presences']);
+              } else if (user.roles[0] === 'Heure suplementaires') {
+                this.router.navigate(['/layouts/presences/heures-supp']);
+              } else if (user.roles[0] === 'Liste de paiements') {
+                this.router.navigate(['/layouts/salaires/liste-paiements']);
+              } else if (user.roles[0] === 'Statuts de paies') {
+                this.router.navigate(['/layouts/salaires/statuts-paies']);
+              } else if (user.roles[0] === 'Relevés de paies') {
+                this.router.navigate(['/layouts/salaires/releve-paie']);
+              } else if (user.roles[0] === 'Avances salaires') {
+                this.router.navigate(['/layouts/salaires/avance-salaire']);
+              } else if (user.roles[0] === 'Primes divers') {
+                this.router.navigate(['/layouts/salaires/primes']);
+              } else if (user.roles[0] === 'Pénalites') {
+                this.router.navigate(['/layouts/salaires/penalites']);
+              } else if (user.roles[0] === 'Près entreprise') {
+                this.router.navigate(['/layouts/salaires/pres-entreprise']);
+              } else if (user.roles[0] === 'Syndicats') {
+                this.router.navigate(['/layouts/personnels/syndicats']);
+              } else if (user.roles[0] === 'Horaires') {
+                this.router.navigate(['/layouts/horaire']);
+              } else if (user.roles[0] === 'Performences') {
+                this.router.navigate(['/layouts/performences']);
+              } else if (user.roles[0] === 'Postes') {
+                this.router.navigate(['/layouts/recrutements/postes']);
+              } else if (user.roles[0] === 'Candidatures') {
+                this.router.navigate(['/layouts/recrutements/candidatures']);
+              } else if (user.roles[0] === 'Emails') {
+                this.router.navigate(['/layouts/mail/inbox']);
+              } else if (user.roles[0] === 'Preferences') {
+                this.router.navigate(['/layouts/preferences/fonction']);
+              } else if (user.roles[0] === 'Reglages') {
+                this.router.navigate(['/layouts/preferences/reglages']);
+              } else if (user.roles[0] === 'Departements') {
+                this.router.navigate(['/layouts/preferences/departement']);
+              } else if (user.roles[0] === 'Sites location') {
+                this.router.navigate(['/layouts/preferences/site-location']);
+              } else if (user.roles[0] === 'Fonctions') {
+                this.router.navigate(['/layouts/preferences/fonction']);
+              } else if (user.roles[0] === 'Services') {
+                this.router.navigate(['/layouts/preferences/services']);
+              } else if (user.roles[0] === 'Titres') {
+                this.router.navigate(['/layouts/preferences/titles']);
+              } else if (user.roles[0] === 'Archives') {
+                this.router.navigate(['/layouts/archives']);
+              }
+            } else {
+              this.router.navigate(['/auth/login']);
+            }
             
             this.isLoading = false;
           },
