a79241b6937ba218831007b1da1f768a6415c905
Kgermando <katakugermain@gmail.com>
2023-12-17 18:22:16 +0100

    add url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,11 @@ const env = process.env.NODE_ENV;
 
 export const environment = {  
   apiURL: (env  === 'production') 
-    ? 'https://wm-rh-api-782e302a0219.herokuapp.com/api'
+    ? 'https://wmrh-api-ad3137eb4651.herokuapp.com/api'
     : 'http://192.168.100.108:3000/api',
 
   publicKey: "BHnrukMOoUpozT0O0LK9g_snE-nSCM_XeoEfbsy3FJO5vJQIAk5TeSYqol0HlvMUU-3poVLx1xNl8nAv14JVoL4",
   privateKey: "EowHD96TortofOIEv_Idh2vidxh52GNSpnE-PYipqD0"
 };
+
+
