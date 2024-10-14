d368fd8f7961d8172671b12fcf41fd31a93145cc
Germain kataku <katakugermain@gmail.com>
2024-10-14 12:24:48 +0100

    add useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -234,8 +234,8 @@ const routes: Routes = [
   
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
+@NgModule({ 
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
